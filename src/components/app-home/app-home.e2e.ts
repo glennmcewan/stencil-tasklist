53853f566fea17aa1450f0fa52c5e4cb7207b644
Glenn McEwan <glenn@web-dev.ninja>
2019-09-10 14:24:57 +0000

    Fix App Home e2e test

--- src/components/app-home/app-home.e2e.ts
+++ src/components/app-home/app-home.e2e.ts
@@ -9,11 +9,11 @@ describe('app-home', () => {
     expect(element).toHaveClass('hydrated');
   });
 
-  it('contains a "Profile Page" button', async () => {
+  it('contains a "Your Tasklist" message', async () => {
     const page = await newE2EPage();
     await page.setContent('<app-home></app-home>');
 
-    const element = await page.find('app-home ion-content ion-button');
-    expect(element.textContent).toEqual('Profile page');
+    const element = await page.find('ion-header ion-title');
+    expect(element.textContent).toEqual('Simple Tasklist');
   });
 });
