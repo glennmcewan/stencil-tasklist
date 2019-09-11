aa57551f26a405076ee74e4c60379623ba1df942
Glenn McEwan <glenn@web-dev.ninja>
2019-09-11 14:02:22 +0000

    Improve specificy of styling (remove shadow dom for todomvc styles)

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -3,15 +3,14 @@ import { Component, h } from '@stencil/core';
 
 @Component({
   tag: 'app-root',
-  styleUrl: 'app-root.css',
-  shadow: true
+  styleUrl: 'app-root.css'
 })
 export class AppRoot {
 
   render() {
     return (
       <div>
-        <header>
+        <header class="page-header">
           <h1>Stencil App Starter</h1>
         </header>
 
@@ -19,7 +18,6 @@ export class AppRoot {
           <stencil-router>
             <stencil-route-switch scrollTopOffset={0}>
               <stencil-route url='/' component='app-home' exact={true} />
-              <stencil-route url='/profile/:name' component='app-profile' />
             </stencil-route-switch>
           </stencil-router>
         </main>
